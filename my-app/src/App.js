a1d32e7e4c8fcf1e24fdc9c7358320d1a991265b
khanhlong11a1@gmail.com <khanhlong11a1@gmail.com>
2021-08-21 14:16:35 +0700

    last commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,7 @@
-
 import './App.css';
-import Todo from './Components/Todo/Todo';
-// import Form from './Components/Form/form';
-// import {Props} from './Components/Props/Props'
 function App() {
   return (
     <div className="App">
-     {/* <Props/> */}
-     {/* <Form/> */}
-     <Todo/>
     </div>
   );
 }
